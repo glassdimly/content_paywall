ae562c243cd998314927477257fc1629e1197620
Jeremy John <jmjohn@riseup.net>
2018-04-18 15:53:09 -0500

    Initial javascript code cleanup. Still messy, but better.

--- js/content_paywall.js
+++ js/content_paywall.js
@@ -1,11 +1,26 @@
-// Add the F*in cookie code directly. No more errors.
+/**
+ * @file
+ * Truncate content based on user role settings from PHP.
+ */
+
+// Different cookies from different libraries were being added. This is a tiny
+// amount of code. Therefore: add the cookie code directly.
 jQuery.cpCookie=function(b,j,m){if(typeof j!="undefined"){m=m||{};if(j===null){j="";m.expires=-1}var e="";if(m.expires&&(typeof m.expires=="number"||m.expires.toUTCString)){var f;if(typeof m.expires=="number"){f=new Date();f.setTime(f.getTime()+(m.expires*24*60*60*1000))}else{f=m.expires}e="; expires="+f.toUTCString()}var l=m.path?"; path="+(m.path):"";var g=m.domain?"; domain="+(m.domain):"";var a=m.secure?"; secure":"";document.cookie=[b,"=",encodeURIComponent(j),e,l,g,a].join("")}else{var d=null;if(document.cookie&&document.cookie!=""){var k=document.cookie.split(";");for(var h=0;h<k.length;h++){var c=jQuery.trim(k[h]);if(c.substring(0,b.length+1)==(b+"=")){d=decodeURIComponent(c.substring(b.length+1));break}}}return d}};
 
+/**
+ * Truncate content based on user role settings.
+ *
+ * @type {Drupal~behavior}
+ *
+ */
 (function($, Drupal, window, document) {
+
   'use strict';
   Drupal.behaviors.content_paywall = {
+
     attach: function(context, settings) {
-      
+
+      // Make reasonable-length variables from settings.
       var contentDeselectors = Drupal.settings.content_paywall.content_deselectors;
       var contentSelectors = Drupal.settings.content_paywall.content_selector;
       var contentRestriction = Drupal.settings.content_paywall.content_restriction_type;
@@ -17,24 +32,26 @@ jQuery.cpCookie=function(b,j,m){if(typeof j!="undefined"){m=m||{};if(j===null){j
 
       $('body').addClass('content-paywall-restriction__' + contentRestriction);
 
-      //check for double breaks
-      var doubleBreakContainers = $(contentSelectors + ' :has(br+br)');
+      // Check for double breaks.
+      var $doubleBreakContainers = $(contentSelectors + ' :has(br+br)');
 
-      if (doubleBreakContainers.length > 0) {
-        //replace double breaks with paragraphs.
-        stripBeakTags($, doubleBreakContainers);
+      if ($doubleBreakContainers.length > 0) {
+        // Replace double breaks with paragraphs.
+        replaceDoubleBreaks($, $doubleBreakContainers);
       }
 
       var has_access = contentRestriction == 'premium' ? false : true;
       // Now, if it's metered, check cookie to see if they've passed the limit.
       if (contentRestriction.substring(0, 7) == "metered") {
         var cookieArray = $.cpCookie('content_paywall_js') ? $.makeArray($.cpCookie('content_paywall_js').split(',')) : [];
+
         // If they've already gotten to read the article grant them access.
         var has_accessed = $.inArray(nid, cookieArray) >= 0 ? true : false;
         var is_over_limit = cookieArray.length >= articleLimit;
         if (is_over_limit && !has_accessed) {
           has_access = false;
         }
+
         // If they haven't read it but now get permanent access, and they're
         // not over the limit, add to cookie.
         if (has_access && !(is_over_limit) && !has_accessed) {
@@ -46,6 +63,7 @@ jQuery.cpCookie=function(b,j,m){if(typeof j!="undefined"){m=m||{};if(j===null){j
           });
         }
       }
+
       else {
         $.cpCookie('content_paywall_js', nid, {
           expires: cookieExpiry,
@@ -53,6 +71,7 @@ jQuery.cpCookie=function(b,j,m){if(typeof j!="undefined"){m=m||{};if(j===null){j
           domain: document.domain
         });
       }
+
       if (!has_access) {
         // Truncate content.
         var paragraphCount = 0;
@@ -66,6 +85,7 @@ jQuery.cpCookie=function(b,j,m){if(typeof j!="undefined"){m=m||{};if(j===null){j
             $(this).remove();
           }
         });
+
         $(contentSelectors).append(accessDeniedMsg);
         $('body').addClass('content-paywall-truncated');
       }
@@ -73,56 +93,74 @@ jQuery.cpCookie=function(b,j,m){if(typeof j!="undefined"){m=m||{};if(j===null){j
   }
 })(jQuery, Drupal, this, this.document);
 
-
-//strips double breaks and wraps them in p tags
-function stripBeakTags($, doubleBreakContainers) {
-  var targetP = null;
-  var justSplit = false;
-  doubleBreakContainers.each( function() {
-    var parentP = $(this);
+/**
+ * Recursive function that strips double breaks and wraps them in "p" tags.
+ *
+ * @param Object $: the jQuery object
+ *
+ * @param doubleBreakContainers the dom content selector and
+ */
+function replaceDoubleBreaks($, $doubleBreakContainers) {
+  var $targetP = null;
+  var shouldSplit = false;
+  $doubleBreakContainers.each( function() {
+    var $parentP = $(this);
     var isFirstPart = true;
-    parentP.contents().each( function(index) {
-      if (justSplit) {
-        justSplit = false;
+
+    $parentP.contents().each( function(index) {
+      if (shouldSplit) {
+        shouldSplit = false;
         // Continue the loop. Otherwise, it would copy an unwanted <br>.
         return true;
       }
-      var thisjNode = $(this);
-      var nextjNode = $(this.nextSibling);
-      if (thisjNode.is("br") && nextjNode.is("br") ) {
+
+      var $thisNode = $(this);
+      var $nextNode = $(this.nextSibling);
+      if ($thisNode.is("br") && $nextNode.is("br") ) {
+
         // Loop through and remove preceding elements, copy out html to to echo in p tag.
         var firstPart = '';
         if (isFirstPart) {
-          parentP.contents().each(function (innerIndex, innerElement) {
+          $parentP.contents().each(function (innerIndex, innerElement) {
             if (innerIndex == index) {
               return false;
             }
+            
             firstPart += outerHTML(innerElement);
             $(innerElement).remove();
           });
+          
           isFirstPart = false;
         }
-        if (targetP == null) {
-          //console.log(firstPart, 'firstPart');
-          parentP.prepend('<p class="firstParagraph">'+firstPart+'</p>');
-          targetP = parentP.find('p.firstParagraph');
+        
+        if ($targetP == null) {
+          $parentP.prepend('<p class="firstParagraph">'+firstPart+'</p>');
+          $targetP = $parentP.find('p.firstParagraph');
         }
-        thisjNode.remove(); //-- Kill this <br>
-        nextjNode.remove(); //-- and the next
+        
+        $thisNode.remove(); // Kill this <br>
+        $nextNode.remove(); // and the next
 
-        //-- Create a new p for any following content
-        targetP.after ('<p></p>');
-        targetP = targetP.next ("p");
-        justSplit = true;
+        // Create a new p for any following content
+        $targetP.after ('<p></p>');
+        $targetP = $targetP.next ("p");
+        shouldSplit = true;
       }
-      else if (targetP) {
-        //-- Move the node to its new home.
-        targetP.append(this);
+
+      else if ($targetP) {
+        // Move the node to its new home.
+        $targetP.append(this);
       }
     });
-  } );
+  });
 }
 
+/**
+ * Return a native outerHTML function or a constructed one.
+ *
+ * @return
+ *   function outerHTML
+ */
 function outerHTML(node) {
   // if IE, Chrome take the internal method otherwise build one.
   return node.outerHTML || (
--- js/content_paywall_debug.js
+++ js/content_paywall_debug.js
@@ -7,13 +7,16 @@
         var value = re.exec(document.cookie);
         return (value != null) ? unescape(value[1]) : null;
       };
+
       console.log(Drupal.settings.content_paywall_debug);
       console.log('Nids accessed (cookie):', getCookie('content_paywall_js'));
+
       var subscriber_access_check = Drupal.settings.content_paywall_debug.subscriber_access_check ? 1 : 0;
       var username = Drupal.settings.content_paywall_debug.username;
       var session = Drupal.settings.content_paywall_debug.session;
       var roles = Drupal.settings.content_paywall_debug.roles;
       var debug_classes = 'subscriber_access_check-' + subscriber_access_check + ' username-' + username + ' session-' + session + ' roles-' + roles;
+
       $('body').addClass(debug_classes);
     }
   };
